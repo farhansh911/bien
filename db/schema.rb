8755b18e3294ff61acb06a50e286afa440676a25
Farhan Sheikh <farhansheikh517@gmail.com>
2023-10-09 00:06:23 +0500

    added ambiance and SEO

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_201522) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_08_185019) do
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_201522) do
     t.string "cusisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end
